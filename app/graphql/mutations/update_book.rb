2de0800202b14742eab0532bb6487e7ba6082d9d
takumi kimura <juventus0901@gmail.com>
2018-02-24 20:24:21 +0900

    Added graphQL update & destroy

--- app/graphql/mutations/update_book.rb
+++ app/graphql/mutations/update_book.rb
@@ -1,4 +1,27 @@
 Mutations::UpdateBook = GraphQL::Relay::Mutation.define do
+  name "UpdateBook"
 
+  input_field :id, !types.ID
+  input_field :name, !types.String
+  input_field :about, !types.String
+
+  return_field :book, !Types::BookType
+
+
+
+  resolve ->(_obj, args, ctx) {
+    begin
+      book = Book.find_by_id(args[:id])
+
+      book.name = args[:name]
+      book.about = args[:about]
+      book.save
+
+      { book: book }
+
+    rescue => e
+      return GraphQL::ExecutionError.new(e.message)
+    end
+  }
 
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,4 +1,6 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name 'Mutation'
   field :CreateBook, field: Mutations::CreateBook.field
+  field :UpdateBook, field: Mutations::UpdateBook.field
+  field :DestroyBook, field: Mutations::DestroyBook.field
 end
