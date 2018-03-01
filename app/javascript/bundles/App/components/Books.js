8b2e99a479a51f6f2c99aee619a37a78c9609514
takumi kimura <juventus0901@gmail.com>
2018-03-01 17:58:03 +0900

    Added Redux form

--- app/javascript/bundles/App/components/Books.js
+++ app/javascript/bundles/App/components/Books.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {Link} from 'react-router-dom'
+import {Field} from 'redux-form'
 import gql from "graphql-tag";
 import {
   PageHeader,
@@ -11,7 +12,10 @@ import {
   FormControl,
   ControlLabel,
   Button,
-  Badge
+  Badge,
+  Grid,
+  Row,
+  Col
 } from 'react-bootstrap'
 
 const fetchAllBooks = gql`
@@ -24,40 +28,32 @@ const fetchAllBooks = gql`
   }
 `
 
+const inputForm = ({input, name, type, placeholder, componentClass}) => {
+  return (
+    <FormControl
+      name={name}
+      type={type}
+      {...input}
+      componentClass={componentClass}
+      placeholder={placeholder}
+    />
+  )
+}
+
 export default class Books extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: {
-        name: '',
-        about: ''
-      }
-    }
   }
 
-  onHandleChange(e) {
-    let data = this.state.data
-    data[e.target.name] = e.target.value
-    this.setState({
-      data: data
-    })
-  }
-
-  onSubmit(e) {
+  onSubmit(values) {
+    const {reset} = this.props
     this.props.createBook({
-      variables: {name: this.state.data.name, about: this.state.data.about},
+      variables: {name: values.name, about: values.about},
       refetchQueries: [{
         query: fetchAllBooks
       }]
     })
-    this.setState({
-      data: {
-        name: '',
-        about: ''
-      }
-    })
-
-    this.refs.form.reset()
+    reset()
   }
 
 
@@ -71,51 +67,51 @@ export default class Books extends React.Component {
   }
 
   render() {
-    const {allBooks} = this.props
+    const {allBooks, handleSubmit} = this.props
     return (
-      <div>
-        <Panel>
-          <Panel.Heading>本の登録</Panel.Heading>
-          <Panel.Body>
+      <Grid>
+        <Row className="show-grid">
+          <Col sm={3} md={3}>
+            <ListGroup>
+              {allBooks && allBooks.length > 0 && allBooks.map((e, key) => {
+                return (
+                  <ListGroupItem key={key}>
+                    {e.name} <Badge onClick={this.onClickDelete.bind(this, e.id)}><i className={'glyphicon glyphicon-remove'}></i></Badge>
+                  </ListGroupItem>
+                )
+              })}
+            </ListGroup>
+          </Col>
+          <Col sm={9} md={9}>
+            <PageHeader>
+              Book登録
+            </PageHeader>
             <form ref={"form"}>
               <FormGroup controlId="formBasicText">
                 <ControlLabel>本の名前を入力</ControlLabel>
-                <FormControl
-                  name={"name"}
+                <Field
+                  component={inputForm}
+                  name="name"
                   type="text"
-                  value={this.state.name}
-                  placeholder="Enter text"
-                  onChange={this.onHandleChange.bind(this)}
+                  placeholder="本の名前を入力"
                 />
               </FormGroup>
               <FormGroup controlId="formControlsTextarea">
-                <ControlLabel>Textarea</ControlLabel>
-                <FormControl
-                  name={"about"}
+                <ControlLabel>本の詳細</ControlLabel>
+                <Field
+                  component={inputForm}
+                  name="about"
                   componentClass="textarea"
-                  placeholder="textarea"
-                  onChange={this.onHandleChange.bind(this)}
+                  placeholder="本の詳細"
                 />
               </FormGroup>
-              <Button onClick={this.onSubmit.bind(this)} bsStyle="primary" bsSize="large" block>
+              <Button onClick={handleSubmit(this.onSubmit.bind(this))} bsStyle="primary" bsSize="large" block>
                 登録する
               </Button>
             </form>
-          </Panel.Body>
-        </Panel>
-        <PageHeader>
-          Books <small>漫画本一覧</small>
-        </PageHeader>
-        <ListGroup>
-          {allBooks && allBooks.length > 0 && allBooks.map((e, key) => {
-            return (
-              <ListGroupItem key={key}>
-                {e.name} <Badge onClick={this.onClickDelete.bind(this, e.id)}><i className={'glyphicon glyphicon-remove'}></i></Badge>
-              </ListGroupItem>
-            )
-          })}
-        </ListGroup>
-      </div>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
