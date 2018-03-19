95fd2c8f623c0f0ccd7d5c71ff3b1b3739eb2c70
takumi kimura <juventus0901@gmail.com>
2018-03-19 22:24:38 +0900

    Fix DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+@category = Category.new
+@category.name = '王道'
+@category.save
+
+@category = Category.new
+@category.name = 'アクション'
+@category.save
+
 @book = Book.new
 @book.name = 'ドラゴンボール'
-@book.about = '探そうぜ！ドラゴンボール'
+@book.author = '鳥山明'
+@book.category_id = "1"
+@book.description = '探そうぜ！ドラゴンボール'
 @book.save
 
 @book = Book.new
 @book.name = 'ワンピース'
-@book.about = '海賊王に俺はなる!'
+@book.author = '尾田栄一郎'
+@book.category_id = "1"
+@book.description = '海賊王に俺はなる!'
 @book.save
